cc214ad9ac9f0c5c0248fad7ca7aba8814245633
AlvaroPM <alvaropavonmartinez7@gmail.com>
2025-02-05 11:46:07 +0100

    Aplicacion Lector base

--- app/src/main/java/com/azrael/qrlector/Dialogs.kt
+++ app/src/main/java/com/azrael/qrlector/Dialogs.kt
@@ -6,11 +6,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
+
 @Composable
 fun ConfirmActionDialog(
     title: String,
     message: String,
-    onConfirm: @Composable () -> Unit,
+    onConfirm: () -> Unit,
     onDismiss: () -> Unit
 ) {
     AlertDialog(
